4317855e4c3d75f67723369d4e85cc230d93e066
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-08 21:58:47 +0530

    Create buttons,append and style them-added buttons in container_1 to scale the grid of boxes in container_2.

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ const btn_container=document.querySelector('.btn_container');
 
 btn_container.style.cssText='display: flex; border: 3px solid grey; justify-content: center;\
 gap: 18px; padding: 12px; margin: 40px; auto;'
+
+const btn_up=document.createElement('button');  //buttons to be placed in container_1 for scaling the grid
+const btn_down=document.createElement('button');
+
+container_1.appendChild(btn_up);
+container_1.appendChild(btn_down);
+
+const btn_scale=document.querySelectorAll('button');
+console.log(btn_scale)
+btn_scale.forEach((btn) => {
+    btn.style.cssText='border: 1px solid blue; color: gold; padding: 6px;\
+    border-radius: 4px;';
+});
+btn_up.textContent='Scale-up';
+btn_down.textContent='Scale-down';
