39928329d97f0aff662b7e2c272bfe236128cc44
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-08 21:36:04 +0530

    Query the div-queried the grand_container classed div and added style to it.

---
+++ script.js
@@ -0,0 +1,3 @@
+const grand_container=document.querySelector('.grand_container');
+grand_container.style.cssText='display: flex; justify-content: center; gap: 16px;\
+border: 3px solid black; margin 30px auto;';
