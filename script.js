ff8d00fa5f69eac6c04bf12c65ab6d0757948414
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-08 21:46:05 +0530

    Add margin and padding to the div's-added some padding to grand_container classed div and some margin to btn_container classed div.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const grand_container=document.querySelector('.grand_container');
 grand_container.style.cssText='display: flex; justify-content: center; gap: 16px;\
-border: 3px solid black; margin 30px auto;';
+border: 3px solid black; margin 30px auto; padding: 24px;';
 
 const container_1=document.querySelector('.container_1');
 const container_2=document.querySelector('.container_2');
@@ -14,4 +14,4 @@ border: 3px solid bisque;'
 const btn_container=document.querySelector('.btn_container');
 
 btn_container.style.cssText='display: flex; border: 3px solid grey; justify-content: center;\
-gap: 18px; padding: 12px;'
+gap: 18px; padding: 12px; margin: 40px; auto;'
