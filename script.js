a44e270472b453246bfcc1b7f91b7f6ef4177310
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-08 21:43:45 +0530

    Query the div and add style to them-queried the div's with class container_1,container_2 and btn_container and added some style to them.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const grand_container=document.querySelector('.grand_container');
 grand_container.style.cssText='display: flex; justify-content: center; gap: 16px;\
 border: 3px solid black; margin 30px auto;';
+
+const container_1=document.querySelector('.container_1');
+const container_2=document.querySelector('.container_2');
+
+container_1.style.cssText='display: flex; flex-direction: column; justify-content: center;\
+gap: 12px; border: 3px solid red; padding: 12px;';
+
+container_2.style.cssText='box-sizing: border-box; width: 486px; height: 486px;\
+border: 3px solid bisque;'
+
+const btn_container=document.querySelector('.btn_container');
+
+btn_container.style.cssText='display: flex; border: 3px solid grey; justify-content: center;\
+gap: 18px; padding: 12px;'
