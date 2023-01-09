7b0d71a48e20d238d10e3afb846a99abc820a782
Syed Faheem <111534462+syedfaheem27@users.noreply.github.com>
2023-01-10 00:23:28 +0530

    Remove borders and change the padding and text color-removed borders of grand_container,container_1 and btn_container and changed the padding and the text color of the buttons.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const grand_container=document.querySelector('.grand_container');
 grand_container.style.cssText='display: flex; justify-content: center; gap: 16px;\
-border: 3px solid black; margin 30px auto; padding: 24px;';
+ margin 30px auto; padding: 24px;';
 
 const container_1=document.querySelector('.container_1');
 const container_2=document.querySelector('.container_2');
 
 container_1.style.cssText='display: flex; flex-direction: column; justify-content: center;\
-gap: 12px; border: 3px solid red; padding: 12px;';
+gap: 12px; padding: 12px;';
 
 container_2.style.cssText='box-sizing: border-box; width: 486px; height: 486px;\
 border: 3px solid black; display: flex; gap: 0; flex-wrap: wrap;\
@@ -14,7 +14,7 @@ align-items: flex-start;  ';
 
 const btn_container=document.querySelector('.btn_container');
 
-btn_container.style.cssText='display: flex; border: 3px solid grey; justify-content: center;\
+btn_container.style.cssText='display: flex;  justify-content: center;\
 gap: 18px; padding: 12px; margin: 40px; auto;'
 
 const btn_up=document.createElement('button');  //buttons to be placed in container_1 for scaling the grid
@@ -26,7 +26,7 @@ container_1.appendChild(btn_down);
 const btn_scale=document.querySelectorAll('button');
 
 btn_scale.forEach((btn) => {
-    btn.style.cssText='border: 1px solid blue; color: gold; padding: 6px;\
+    btn.style.cssText='border: 1px solid blue; color: red; padding: 10px;\
     border-radius: 4px;';
 });
 btn_up.textContent='Scale-up';
@@ -37,8 +37,8 @@ btn_down.textContent='Scale-down';
 const btn_option=[];
 for(let i=0;i<4;i++) {
     btn_option[i]=document.createElement('button');
-    btn_option[i].style.cssText='border: 1px solid blue; color: gold; \
-    background-color: bisque; padding: 6px; border-radius: 4px;';
+    btn_option[i].style.cssText='border: 1px solid blue; color: red; \
+    background-color: bisque; padding: 10px; border-radius: 4px;';
     btn_container.appendChild(btn_option[i]);
 }
 btn_option[0].textContent='Grey-Black';
